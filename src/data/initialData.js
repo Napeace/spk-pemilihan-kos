04119b903223d296609293048de3ef51dc7670f4
Napeace <najminafis1504@gmail.com>
2025-11-12 10:16:01 +0700

    sedikit perubahan pada konsistensi data

--- src/data/initialData.js
+++ src/data/initialData.js
@@ -1,13 +1,13 @@
 export const initialKosData = [
-  { id: 1, nama: "Kos Mawar", harga: 800000, fasilitas: 5, waktuTempuh: 10, luasKamar: 20, keamanan: 5 },
-  { id: 2, nama: "Kos Melati", harga: 600000, fasilitas: 3, waktuTempuh: 15, luasKamar: 12, keamanan: 4 },
+  { id: 1, nama: "Kos Mawar", harga: 800000, fasilitas: 5, waktuTempuh: 10, luasKamar: 12, keamanan: 5 },
+  { id: 2, nama: "Kos Melati", harga: 600000, fasilitas: 3, waktuTempuh: 15, luasKamar: 9, keamanan: 4 },
   { id: 3, nama: "Kos Anggrek", harga: 1000000, fasilitas: 5, waktuTempuh: 5, luasKamar: 25, keamanan: 5 },
   { id: 4, nama: "Kos Dahlia", harga: 500000, fasilitas: 2, waktuTempuh: 20, luasKamar: 9, keamanan: 3 },
   { id: 5, nama: "Kos Sakura", harga: 750000, fasilitas: 4, waktuTempuh: 12, luasKamar: 16, keamanan: 4 },
-  { id: 6, nama: "Kos Teratai", harga: 900000, fasilitas: 4, waktuTempuh: 8, luasKamar: 20, keamanan: 5 },
-  { id: 7, nama: "Kos Kenanga", harga: 550000, fasilitas: 3, waktuTempuh: 18, luasKamar: 12, keamanan: 3 },
+  { id: 6, nama: "Kos Teratai", harga: 900000, fasilitas: 4, waktuTempuh: 8, luasKamar: 12, keamanan: 5 },
+  { id: 7, nama: "Kos Kenanga", harga: 550000, fasilitas: 3, waktuTempuh: 18, luasKamar: 9, keamanan: 3 },
   { id: 8, nama: "Kos Tulip", harga: 850000, fasilitas: 4, waktuTempuh: 10, luasKamar: 16, keamanan: 4 },
-  { id: 9, nama: "Kos Cempaka", harga: 700000, fasilitas: 3, waktuTempuh: 14, luasKamar: 16, keamanan: 4 },
+  { id: 9, nama: "Kos Cempaka", harga: 700000, fasilitas: 3, waktuTempuh: 14, luasKamar: 12, keamanan: 4 },
   { id: 10, nama: "Kos Seruni", harga: 650000, fasilitas: 3, waktuTempuh: 16, luasKamar: 12, keamanan: 3 }
 ];
 
