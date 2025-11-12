f6362b963887614f63b63833ee93bde6c862c429
Napeace <najminafis1504@gmail.com>
2025-11-12 10:19:56 +0700

    sedikit perbaikan pada data

--- src/data/initialData.js
+++ src/data/initialData.js
@@ -1,8 +1,8 @@
 export const initialKosData = [
   { id: 1, nama: "Kos Mawar", harga: 800000, fasilitas: 5, waktuTempuh: 10, luasKamar: 12, keamanan: 5 },
   { id: 2, nama: "Kos Melati", harga: 600000, fasilitas: 3, waktuTempuh: 15, luasKamar: 9, keamanan: 4 },
-  { id: 3, nama: "Kos Anggrek", harga: 1000000, fasilitas: 5, waktuTempuh: 5, luasKamar: 25, keamanan: 5 },
-  { id: 4, nama: "Kos Dahlia", harga: 500000, fasilitas: 2, waktuTempuh: 20, luasKamar: 9, keamanan: 3 },
+  { id: 3, nama: "Kos Anggrek", harga: 1000000, fasilitas: 5, waktuTempuh: 5, luasKamar: 12, keamanan: 5 },
+  { id: 4, nama: "Kos Dahlia", harga: 500000, fasilitas: 2, waktuTempuh: 20, luasKamar: 25, keamanan: 3 },
   { id: 5, nama: "Kos Sakura", harga: 750000, fasilitas: 4, waktuTempuh: 12, luasKamar: 16, keamanan: 4 },
   { id: 6, nama: "Kos Teratai", harga: 900000, fasilitas: 4, waktuTempuh: 8, luasKamar: 12, keamanan: 5 },
   { id: 7, nama: "Kos Kenanga", harga: 550000, fasilitas: 3, waktuTempuh: 18, luasKamar: 9, keamanan: 3 },
